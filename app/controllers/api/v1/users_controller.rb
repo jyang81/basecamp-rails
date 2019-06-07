1abb219e797ea2cdc81360a3f0d887618f19bc6c
jyang81 <joe.yang.design@gmail.com>
2019-06-07 14:17:53 -0700

    redoing previous commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,24 @@
 class Api::V1::UsersController < ApplicationController
 
-  before_action :find_user, only: [:update, :destroy]
+  before_action :find_user, only: [:update, :destroy, :show]
+  skip_before_action :authorized, only: [:create]
 
     def index
       @users = User.all
       render json: @users
     end
 
-    def new
-      @user = User.new
+    def profile
+      render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
     def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :accepted
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       else
-        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+        render json: { errors: 'failed to create user' }, status: :not_acceptable
       end
     end
 
@@ -36,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation, :school, :course_id, :start_date, :end_date)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :school, :course_id, :start_date, :end_date)
     end
 
     def find_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,9 @@ Note.destroy_all
 NoteCategory.destroy_all
 
 Course.create(name: 'Software Engineering Immersive', start_date: '2019-03-11', end_date: '2019-06-21')
+Course.create(name: 'Data Science', start_date: '2019-04-22', end_date: '2019-07-12')
 
-User.create(name: 'Admin', password: '1234', password_confirmation: '1234', school: 'Flatiron School', start_date: '2019-03-11', end_date: '2019-06-21', course_id: 1)
+User.create(name: 'Admin', email: 'admin@admin.com', password: '1234', password_confirmation: '1234', school: 'Flatiron School', start_date: '2019-03-11', end_date: '2019-06-21', course_id: 1)
 
 ToDo.create(item: 'Write blog #1', category: 'Blogs', due_date: Date.today + 7, user_id: 1)
 ToDo.create(item: 'Finalize FSP topic', category: 'FSP', due_date: Date.today + 10, user_id: 1)
@@ -24,5 +25,6 @@ ToDo.create(item: 'Finalize FSP topic', category: 'FSP', due_date: Date.today + 
 NoteCategory.create(title: 'Project Ideas', user_id: 1)
 
 Note.create(content: 'Recycling App', note_category_id: 1)
+Note.create(content: 'Trivia Game', note_category_id: 1)
 
 Mood.create(rating: 5, date: Date.today, user_id: 1)
